9da6a5fa9a8bf3fecc6dc1e6c1e64cc9647161d2
nszul44 <nicholas.szul04@myhunter.cuny.edu>
2025-11-25 21:17:24 -0500

    token fixes

--- frontend/client/src/config.js
+++ frontend/client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API_URL = process.env.REACT_APP_API_URL || "https://what-to-do-052g.onrender.com/";
 
 export default API_URL;
