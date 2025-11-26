310d7e6f11a7f01eff36b23df3c7145dcbf19321
nszul44 <nicholas.szul04@myhunter.cuny.edu>
2025-11-25 21:21:23 -0500

    token fixes

--- frontend/client/src/config.js
+++ frontend/client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || "https://what-to-do-052g.onrender.com/";
+const API_URL = process.env.REACT_APP_API_URL || "https://what-to-do-052g.onrender.com";
 
 export default API_URL;
