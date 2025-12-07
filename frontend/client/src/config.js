3754f8ea78bda7f391d0ae7faa4f365f118ea6ea
nszul44 <nicholas.szul04@myhunter.cuny.edu>
2025-12-07 02:15:21 -0500

    render updates

--- frontend/client/src/config.js
+++ frontend/client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API_URL = process.env.REACT_APP_API_URL || "https://what-to-do-z3nn.onrender.com";
 
 export default API_URL;
